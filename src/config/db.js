29dc707e4092d3f2b909a365f3a6c805ce858484
bishal-git-repo <gojo.saitama2546@gmail.com>
2026-04-05 23:46:31 +0530

    fix db bug

--- src/config/db.js
+++ src/config/db.js
@@ -16,7 +16,8 @@ const poolConfig = {
   // Security settings
   multipleStatements: false,
   dateStrings: true,
-  collation: 'utf8mb4_unicode_ci',              // ← add this
+  collation: 'UTF8MB4_UNICODE_CI',              // ← add this
+  charset: 'UTF8MB4_UNICODE_CI',              // ← add this
   timezone: '+00:00', 
 };
 
