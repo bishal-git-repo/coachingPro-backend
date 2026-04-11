bbebc17315fdf0342bea700183003285d2eb99bf
bishal-git-repo <gojo.saitama2546@gmail.com>
2026-04-11 23:10:16 +0530

    fix email code in email.service file

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -8,8 +8,8 @@ function getTransporter() {
   if (!transporter) {
     transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST || 'smtp.gmail.com',
-      port: parseInt(process.env.SMTP_PORT) || 587,
-      secure: false,
+      // port: parseInt(process.env.SMTP_PORT) || 587,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
